c4e8403448ce1648d7750d4bb4022599cee4df62
20013555 <20013555@LAPTOP-I4BDBIKM>
2021-08-10 14:07:40 +0800

    Update Menu

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,25 @@
 
 public class C206_CaseStudy {
+	
+	private static final int OPTION_QUIT = 6;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Have fun!");
+		
+		int option = 0;
+		
+		while (option != OPTION_QUIT) {
+			C206_CaseStudy.menu();
+			option = Helper.readInt("Enter an option > ");
+		}
 	}
-
+	private static void menu() {
+		System.out.println("1. Customer");
+		System.out.println("2. Package");
+		System.out.println("3. Request for quote");
+		System.out.println("4.Quotation");
+		System.out.println("5. Appointment");
+	}
+		
 }
+
